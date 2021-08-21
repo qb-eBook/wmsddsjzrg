1d02f7611ece0d1bf2baf485234bb4bbbe33fe2e
maqingbo <wmaqingbo@163.com>
2021-08-21 14:37:39 +0800

    feat: 郭本禹/方红版本制作完成；

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,17 +7,29 @@ module.exports = {
     ['link', { rel: 'icon', href: '/images/icon.png' }]
   ],
   themeConfig: {
+    nav: [
+      { text: '冯川-版本', link: '/version_1/01.md' },
+      { text: ' 郭本禹/方红-版本', link: '/version_2/01.md' }
+    ],
     // displayAllHeaders: true,
     lastUpdated: '上次更新',
     sidebarDepth: 2,
     sidebar: {
-      '/chapters/': [
+      '/version_1/': [
         '01.md', '02.md', '03.md',
         '04.md', '05.md', '06.md',
         '07.md', '08.md', '09.md',
         '10.md', '11.md', '12.md',
         '13.md', '14.md', '15.md',
         '16.md'
+      ],
+      '/version_2/': [
+        '01.md', '02.md', '03.md',
+        '04.md', '05.md', '06.md',
+        '07.md', '08.md', '09.md',
+        '10.md', '11.md', '12.md',
+        '13.md', '14.md', '15.md',
+        '16.md', '17.md', '18.md',
       ]
     }
   }
--- docs/README.md
+++ docs/README.md
@@ -4,6 +4,6 @@ heroImage: /images/cover.png
 heroText: 我们时代的神经症人格
 tagline: 卡伦 · 霍妮
 actionText: 开始阅读
-actionLink: /chapters/01/
+actionLink: /version_1/01/
 # footer: MIT Licensed | Copyright © 2018-present 上野千鹤子
 ---
